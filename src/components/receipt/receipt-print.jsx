6d6ac57bcac4cef507c8e6f25b2fb98ee917033f
Rashon <rashon@hava.bz>
2020-11-17 07:55:21 +0300

    build build for receipt i hope it works

--- src/components/receipt/receipt-print.jsx
+++ src/components/receipt/receipt-print.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {formatDate} from '../../services/utility';
+import './receipt-print.scss';
 
 export default function ReceiptPrint(props) {
   const {payment, cartItems, onAfterPrint} = props;
@@ -9,121 +10,81 @@ export default function ReceiptPrint(props) {
     onAfterPrint();
   };
   return (
-    <div
-      className='container-fluid '
-      style={{
-        fontWeight: 'bolder',
-      }}
-    >
-      <div className='card'>
-        <div className='card-header row justify-content-end hidden-print'>
-          <button className='btn btn-primary' onClick={handlePrint}>
-            Confirm Print
-          </button>
-        </div>
-        <div className='mb-1'>
-          <hr />
-          <p className='text-center'>
-            <b className='text-center text-dark'>CUSTOMER BILL</b>
-          </p>
-          <p className='text-center '>
-            <strong className='text-center text-dark text-uppercase'>
-              SCRATCH KITCHEN LTD
-            </strong>
-          </p>
-          <p className='text-dark text-center'>
-            {formatDate(payment.created_at)}
-          </p>
-          <p className='text-center '>
-            <small className='text-dark'>
-              <b>P.O.BOX 102358-00101 NRB TELEPHONE: 0791 482 995/0756936852</b>
-            </small>
-          </p>
-          <div className='border-bottom border-top border-secondary  row'>
-            <div className='col'>
-              <p className='text-dark'>
-                <span className='text-secondary'>Bill No</span>: {payment.id}
-              </p>
-            </div>
-            <div className='col'>
-              <p className='text-dark'>
-                <span className='text-dark'>Buy Goods till</span>: {'4028177'}
-              </p>
-            </div>
-            {payment.transaction_code ? (
-              <div className='col'>
-                <p className='text-dark'>
-                  <span className='text-dark'>Reference Number</span>:
-                  {payment.transaction_code}
-                </p>
-              </div>
-            ) : null}
+    <>
+      <div class='ticket'>
+        <p class='centered'>
+          SCRATCH KITCHEN LTD
+          <br />
+          CUSTOMER BILL
+          <br />
+          {formatDate(payment.created_at)}
+          <br />
+          <small>
+            <b>P.O.BOX 102358-00101 NRB TELEPHONE: 0791 482 995/0756936852</b>
+          </small>
+          <br />
+          Till No. - {'4028177'}
+        </p>
+        <table>
+          <thead>
+            <tr>
+              <th class='description'>Good</th>
+              <th class='quantity'>Q.</th>
+              <th class='price1 centered'>@</th>
+              <th class='price2'>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems.map((item) => (
+              <tr>
+                <td className='description'>{item.name}</td>
+                <td className='quantity'>{item.quantity}</td>
+                <td className='price1 centered'>{item.price.amount}</td>
+                <td className='price2 centered'>
+                  {item.price.amount * item.quantity}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <div
+          className='mt-1 border-top border-dark'
+          style={{fontSize: '12px', padding: '12px'}}
+        >
+          <div className=' row justify-content-between text-dark'>
+            <span className='text-uppercase'>Total Amount</span>
+            <span>{payment.total}</span>
+          </div>
+          <div className=' row justify-content-between text-dark'>
+            <span className='text-uppercase text-dark'>
+              {payment.payment_method}
+            </span>
+            <span>{'Ksh ' + payment.customer_pay}</span>
+          </div>
+          <div className=' row justify-content-between text-dark'>
+            <span className='text-uppercase text-dark'>Change</span>
+            <span>{'Ksh ' + payment.customer_change}</span>
           </div>
         </div>
-        <div className='card-body p-0'>
-          <div className='table-responsive'>
-            <table
-              className='table table-bordered  border-darken-4'
-              style={{border: '1px solid #000'}}
-            >
-              <thead>
-                <th>Item Name</th>
-                <th>Qty</th>
-                <th>Price</th>
-                <th>Total</th>
-              </thead>
-              <tbody>
-                {cartItems.map((item) => (
-                  <tr key={item.id}>
-                    <td className='font-weight-bolder'>{item.name}</td>
-                    <td className='font-weight-bolder'>{item.quantity}</td>
-                    <td className='font-weight-bolder'>{item.price.amount}</td>
-                    <td className='font-weight-bolder'>
-                      {item.price.amount * item.quantity}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <div
-            className='mt-1 pl-4 pr-4  border-dark mb-1 '
-            style={{borderStyle: 'dotted'}}
-          >
-            <div className=' row justify-content-between text-dark'>
-              <span className='text-uppercase'>Total Amount</span>
-              <span>{payment.total}</span>
-            </div>
-            <div className=' row justify-content-between text-dark'>
-              <span className='text-uppercase text-dark'>
-                {payment.payment_method}
-              </span>
-              <span>{'Ksh ' + payment.customer_pay}</span>
-            </div>
-            <div className=' row justify-content-between text-dark'>
-              <span className='text-uppercase text-dark'>Change</span>
-              <span>{'Ksh ' + payment.customer_change}</span>
-            </div>
-          </div>
-          <div className='mt-2'>
-            <h6 className='text-center font-weight-bolder'>
-              SERVED BY {payment.sold_by_text}
-            </h6>
-          </div>
+        <div className='mt-2'>
+          <h6 className='text-center font-weight-bolder text-uppercase'>
+            SERVED BY {payment.sold_by_text}
+          </h6>
         </div>
-        <div className='card-footer p-0'>
-          <p className='text-center text-uppercase text-dark'>
-            <small>
-              <b>FOLLOW US ON ig@scratchkitchen for COMPLAINTS AND FEEDBACK</b>
-            </small>
-          </p>
-          <p className='text-center text-uppercase text-dark'>
-            <small>
-              <b> askscratchkitchen@gmail.com</b>
-            </small>
-          </p>
-        </div>
+
+        <p class='centered border-top border-dark' style={{fontSize: '12px'}}>
+          Thanks for your purchase!
+          <br />
+          askscratchkitchen@gmail.com /ig@scratchkitchen
+        </p>
       </div>
-    </div>
+      <div className='w-100 row justify-content-end hidden-print fixed-bottom'>
+        <button className='btn btn-primary' onClick={handlePrint}>
+          <i className='fa fa-print'></i>
+          Confirm Print
+        </button>
+      </div>
+    </>
   );
 }
