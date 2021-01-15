e9ed81f8213b4040b9e5c36930b742b49ef4a116
Rashon <rashon@hava.bz>
2021-01-15 20:40:37 +0300

    Adding the creditors list

--- src/pages/employee/employee-sales.jsx
+++ src/pages/employee/employee-sales.jsx
@@ -1,9 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {trackPromise} from 'react-promise-tracker';
-import {Link} from 'react-router-dom';
 import Counter from '../../components/counter/counter';
 import SelectPeriod from '../../components/select-period/select-period';
-import ToolTipElement from '../../components/tooltip/tooltip-element';
 import {
   handleError,
   handleSuccess,
@@ -11,19 +9,22 @@ import {
   sendPostRequest,
 } from '../../services/api-handle';
 import {time_periods} from '../../services/constants';
-import {
-  formatDate,
-  formatUrl,
-  getFormattedAmount,
-  getFromLocal,
-} from '../../services/utility';
+import {formatUrl, getFromLocal} from '../../services/utility';
+import CreditorsList from './creditors-list';
+import SalesList from './sales-list';
 import withEmployeeValidation from './with-employee-validation';
 
+const pages = {
+  SALES: 1,
+  EXPENSES: 2,
+  CREDITORS: 3,
+};
 function EmployeeSales(props) {
   const currentUser = getFromLocal('currentUser');
   const [sales, setSales] = useState([]);
   const [totals, setTotals] = useState({});
   const [period, setPeriod] = useState(time_periods.TODAY.value);
+  const [page, setPage] = useState(pages.SALES);
 
   const updateData = () => {
     let url = formatUrl('sales', {sold_by: currentUser.id, period});
@@ -102,105 +103,9 @@ function EmployeeSales(props) {
             </div>
           </div>
         </div>
-        <div className='card w-100'>
-          <div className='card-body'>
-            <div className='table-responsive'>
-              <table
-                className='table display table-hover table-striped datatable'
-                style={{width: '100%'}}
-              >
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Date</th>
-                    <th>Number of goods</th>
-                    <th>Total amount</th>
-                    <th>Payment Type</th>
-                    <th>
-                      Customer Pay
-                      <br />
-                      Customer Change
-                    </th>
-                    <th>Status</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {sales.map((sale, index) => (
-                    <tr key={sale.id}>
-                      <td>{++index}</td>
-                      <td>
-                        <Link to={`/employee/sales/view/${sale.id}`}>
-                          {formatDate(sale.created_at)}
-                        </Link>
-                      </td>
-                      <td>{sale.goods_count}</td>
-                      <td>
-                        <ToolTipElement
-                          tooltip={
-                            sale.mpesa_pay
-                              ? `Mpesa Pay- ${sale.mpesa_pay}. Cash Pay-${sale.cash_pay}`
-                              : ''
-                          }
-                        >
-                          <span>{sale.total}</span>
-                        </ToolTipElement>
-                      </td>
-                      <td>{sale.payment_method}</td>
-                      <td>
-                        {getFormattedAmount(sale.customer_pay, 1)}
-                        <br />
-                        {getFormattedAmount(sale.customer_change, 1)}
-                      </td>
-                      <td>
-                        {sale.status !== 1 && (
-                          <>
-                            <button
-                              className='btn btn-danger ml-1'
-                              onClick={() => handleDelete(sale.id)}
-                            >
-                              <i className='fa fa-trash mr-2'></i>
-                              Delete
-                            </button>
-                            <button
-                              className='btn btn-info ml-1'
-                              onClick={() =>
-                                props.history.push(
-                                  `/employee/dashboard/${place}/edit/${sale.id}`
-                                )
-                              }
-                            >
-                              <i className='fa fa-edit mr-2'></i>
-                              Edit
-                            </button>
-                          </>
-                        )}
-                        {sale.status === 2 && (
-                          <ToolTipElement
-                            tooltip={`Edited by ${
-                              sale.updated_by
-                            } at ${formatDate(sale.updated_at)}`}
-                          >
-                            <div className='badge badge-success'>Edited</div>
-                          </ToolTipElement>
-                        )}
 
-                        {sale.status === 1 && (
-                          <ToolTipElement
-                            tooltip={`Deleted by ${
-                              sale.updated_by
-                            } at ${formatDate(sale.updated_at)}`}
-                          >
-                            <div className='badge badge-danger'>Deleted</div>
-                          </ToolTipElement>
-                        )}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+        <SalesList sales={sales} handleDelete={handleDelete} place={place} />
+        <CreditorsList period={period} currentUser={currentUser} />
       </div>
     </div>
   );
---
+++ src/pages/employee/sales-list.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import ToolTipElement from '../../components/tooltip/tooltip-element';
+import {formatDate, getFormattedAmount} from '../../services/utility';
+
+export default function SalesList(props) {
+  const {sales, handleDelete, place} = props;
+  return (
+    <div className='card w-100'>
+      <div className='card-body'>
+        <div className='table-responsive'>
+          <table
+            className='table display table-hover table-striped datatable'
+            style={{width: '100%'}}
+          >
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Date</th>
+                <th>Number of goods</th>
+                <th>Total amount</th>
+                <th>Payment Type</th>
+                <th>
+                  Customer Pay
+                  <br />
+                  Customer Change
+                </th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sales.map((sale, index) => (
+                <tr key={sale.id}>
+                  <td>{++index}</td>
+                  <td>
+                    <Link to={`/employee/sales/view/${sale.id}`}>
+                      {formatDate(sale.created_at)}
+                    </Link>
+                  </td>
+                  <td>{sale.goods_count}</td>
+                  <td>
+                    <ToolTipElement
+                      tooltip={
+                        sale.mpesa_pay
+                          ? `Mpesa Pay- ${sale.mpesa_pay}. Cash Pay-${sale.cash_pay}`
+                          : ''
+                      }
+                    >
+                      <span>{sale.total}</span>
+                    </ToolTipElement>
+                  </td>
+                  <td>{sale.payment_method}</td>
+                  <td>
+                    {getFormattedAmount(sale.customer_pay, 1)}
+                    <br />
+                    {getFormattedAmount(sale.customer_change, 1)}
+                  </td>
+                  <td>
+                    {sale.status !== 1 && (
+                      <>
+                        <button
+                          className='btn btn-danger ml-1'
+                          onClick={() => handleDelete(sale.id)}
+                        >
+                          <i className='fa fa-trash mr-2'></i>
+                          Delete
+                        </button>
+                        <button
+                          className='btn btn-info ml-1'
+                          onClick={() =>
+                            props.history.push(
+                              `/employee/dashboard/${place}/edit/${sale.id}`
+                            )
+                          }
+                        >
+                          <i className='fa fa-edit mr-2'></i>
+                          Edit
+                        </button>
+                      </>
+                    )}
+                    {sale.status === 2 && (
+                      <ToolTipElement
+                        tooltip={`Edited by ${sale.updated_by} at ${formatDate(
+                          sale.updated_at
+                        )}`}
+                      >
+                        <div className='badge badge-success'>Edited</div>
+                      </ToolTipElement>
+                    )}
+
+                    {sale.status === 1 && (
+                      <ToolTipElement
+                        tooltip={`Deleted by ${sale.updated_by} at ${formatDate(
+                          sale.updated_at
+                        )}`}
+                      >
+                        <div className='badge badge-danger'>Deleted</div>
+                      </ToolTipElement>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
