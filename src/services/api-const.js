f331af4577e762a87fdc876bdc1100591b720edb
Rashon <rashon@hava.bz>
2021-01-23 05:30:29 +0300

    initial commit for test

--- src/services/api-const.js
+++ src/services/api-const.js
@@ -1,2 +1,3 @@
-export const API_URL = 'https://posbackend.sk.co.ke/api';
+// export const API_URL = 'https://posbackend.sk.co.ke/api';
+export const API_URL = 'http://posbackend.pinpointwriters.com/public/api';
 // export const API_URL = 'http://127.0.0.1:8000/api/';
