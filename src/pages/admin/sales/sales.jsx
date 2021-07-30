a167a71c77524bb72861fe3cd157efe6f57d40a2
Rashon <rashon@hava.bz>
2021-07-30 08:14:58 +0300

    Default to today

--- src/pages/admin/sales/sales.jsx
+++ src/pages/admin/sales/sales.jsx
@@ -1,46 +1,46 @@
-import React, {Component} from 'react';
-import {trackPromise} from 'react-promise-tracker';
-import {Link} from 'react-router-dom';
-import Counter from '../../../components/counter/counter';
-import Pagination from '../../../components/pagination/pagination';
-import SelectPeriod from '../../../components/select-period/select-period';
-import ToolTipElement from '../../../components/tooltip/tooltip-element';
+import React, { Component } from "react";
+import { trackPromise } from "react-promise-tracker";
+import { Link } from "react-router-dom";
+import Counter from "../../../components/counter/counter";
+import Pagination from "../../../components/pagination/pagination";
+import SelectPeriod from "../../../components/select-period/select-period";
+import ToolTipElement from "../../../components/tooltip/tooltip-element";
 import {
   handleError,
   handleSuccess,
   sendGetRequest,
   sendPostRequest,
-} from '../../../services/api-handle';
-import {time_periods} from '../../../services/constants';
+} from "../../../services/api-handle";
+import { time_periods } from "../../../services/constants";
 import {
   formatDate,
   formatUrl,
   getFormattedAmount,
   getFromLocal,
-} from '../../../services/utility';
-import withTemplate from '../with-template';
+} from "../../../services/utility";
+import withTemplate from "../with-template";
 
 class Sales extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sales: {data: []},
-      period: time_periods.ALL_TIME.value,
+      sales: { data: [] },
+      period: time_periods.TODAY.value,
       totals: {},
     };
   }
 
   updateData = (page) => {
-    const {period} = this.state;
+    const { period } = this.state;
     trackPromise(
       handleError(
-        handleSuccess(sendGetRequest(formatUrl('sales', {period, page}))).then(
-          (res) => {
-            let {sales, totals} = res.data;
-            console.log(totals);
-            this.setState({sales, totals});
-          }
-        )
+        handleSuccess(
+          sendGetRequest(formatUrl("sales", { period, page }))
+        ).then((res) => {
+          let { sales, totals } = res.data;
+          console.log(totals);
+          this.setState({ sales, totals });
+        })
       )
     );
   };
@@ -48,14 +48,14 @@ class Sales extends Component {
     this.updateData();
   }
   handleDelete = (id) => {
-    let currentUser = getFromLocal('currentUser');
+    let currentUser = getFromLocal("currentUser");
     const updated_by = `${currentUser.firstname} ${currentUser.lastname}`;
 
     trackPromise(
       handleError(
         handleSuccess(
-          sendPostRequest(`sales/${id}/delete`, {sale_id: id, updated_by}),
-          'Sale deleted successfully'
+          sendPostRequest(`sales/${id}/delete`, { sale_id: id, updated_by }),
+          "Sale deleted successfully"
         ).then((res) => {
           this.updateData();
         })
@@ -64,54 +64,54 @@ class Sales extends Component {
   };
 
   updateFilter = (obj) => {
-    this.setState({...this.state, ...obj}, () => this.updateData());
+    this.setState({ ...this.state, ...obj }, () => this.updateData());
   };
   render() {
-    const {sales, period, totals} = this.state;
+    const { sales, period, totals } = this.state;
     return (
-      <div className='mt-3'>
+      <div className="mt-3">
         <h2>Sales</h2>
-        <div className='w-100 row justify-content-end mb-2'>
-          <div className='form-group'>
+        <div className="w-100 row justify-content-end mb-2">
+          <div className="form-group">
             <SelectPeriod
               selectedPeriod={period}
-              updatePeriod={(period) => this.updateFilter({period})}
+              updatePeriod={(period) => this.updateFilter({ period })}
             />
           </div>
         </div>
-        <div className='card-body'>
-          <div className='row justify-content-start'>
+        <div className="card-body">
+          <div className="row justify-content-start">
             <Counter
-              title='Total Sales'
-              icon='fa fa-shopping-cart'
+              title="Total Sales"
+              icon="fa fa-shopping-cart"
               displayValue={totals.total_sales || 0}
             />
             <Counter
-              title='Total Expenses'
-              icon='fa fa-dollar'
+              title="Total Expenses"
+              icon="fa fa-dollar"
               displayValue={totals.total_expenses || 0}
             />
             <Counter
-              title='Total Credits in Ksh'
-              icon='fa fa-exclamation-circle'
+              title="Total Credits in Ksh"
+              icon="fa fa-exclamation-circle"
               displayValue={totals.total_credit_amount || 0}
             />
             <Counter
-              title='Mpesa '
-              icon='fa fa-dollar'
+              title="Mpesa "
+              icon="fa fa-dollar"
               displayValue={totals.mpesa_pay || 0}
             />
             <Counter
-              title='Cash'
-              icon='fa fa-money-bill-wave'
+              title="Cash"
+              icon="fa fa-money-bill-wave"
               displayValue={totals.cash_pay || 0}
             />
           </div>
         </div>
-        <div className='table-responsive'>
+        <div className="table-responsive">
           <table
-            className='table display table-hover table-striped datatable'
-            style={{width: '100%'}}
+            className="table display table-hover table-striped datatable"
+            style={{ width: "100%" }}
           >
             <thead>
               <tr>
@@ -131,8 +131,8 @@ class Sales extends Component {
                 <tr key={sale.id}>
                   <td>{++index}</td>
                   <td>
-                    <Link to={'/admin/sales/view/' + sale.id}>
-                      {sale.firstname + ' ' + sale.lastname}
+                    <Link to={"/admin/sales/view/" + sale.id}>
+                      {sale.firstname + " " + sale.lastname}
                     </Link>
                   </td>
                   <td>{sale.payment_method}</td>
@@ -148,7 +148,7 @@ class Sales extends Component {
                           sale.updated_at
                         )}`}
                       >
-                        <div className='badge badge-danger'>Deleted</div>
+                        <div className="badge badge-danger">Deleted</div>
                       </ToolTipElement>
                     )}
                     {sale.status === 2 && (
@@ -157,7 +157,7 @@ class Sales extends Component {
                           sale.updated_at
                         )}`}
                       >
-                        <div className='badge badge-success'>Edited</div>
+                        <div className="badge badge-success">Edited</div>
                       </ToolTipElement>
                     )}
                   </td>
